874718adc1b0b193504fc60462b8f773ddba6416
liangweile <liangweile1994@163.com>
2020-11-26 08:41:00 +0800

    写完线程汇编切换，在写main函数调用程序

--- User/app.c
+++ User/app.c
@@ -1,22 +1,37 @@
+#include "os.h"
+#include "ARMCM3.h"
 
 
-#define		TASK1_STK_SIZE				128
-#define 	TASK2_STK_SIZE				128
+#define		TASK1_STK_SIZE				20
+#define 		TASK2_STK_SIZE				20
 
-static		CPU_STK				Task1Stk[TASK1_STK_SIZE];
-static		CPU_STK				Task2Stk[TASK2_STK_SIZE];
+static			CPU_STK			Task1Stk[TASK1_STK_SIZE];
+static			CPU_STK			Task2Stk[TASK2_STK_SIZE];
 
-static		OS_TCB				Task1TCB;
+static			OS_TCB				Task1TCB;
 static 		OS_TCB				Task2TCB;
 
+void Task1(void *p_arg);
+void Task2(void *p_arg);
+void delay(uint32_t count);
 
 
 uint32_t flag1;
 uint32_t flag2;
 
+
 OSRdyList[0].HeadPtr = &Task1TCB;
 OSRdyList[1].HeadPtr = &Task2TCB;
 
+
+int main(void)
+{
+	OS_ERR	err;
+	OSInit(&err);
+	OSTaskCreate((OS_TCB*)		&Task1TCB,
+	
+	)
+}
 
 void Task1(void *p_arg)
 {
--- User/uCOS-III/Ports/os_cpu_a.s
+++ User/uCOS-III/Ports/os_cpu_a.s
@@ -1,3 +1,10 @@
+IMPORT		OSTCBCurPtr
+IMPORT		OSTCBHighRdyPtr
+	
+EXPORT		OSStartHighRdy
+EXPORT		PendSV_Handler
+
+
 NVIC_INT_CTRL EQU 0xE000ED04 ; 中断控制及状态寄存器 SCB_ICSR。
 NVIC_SYSPRI14 EQU 0xE000ED22 ; 系统优先级寄存器 SCB_SHPR3：
 						     ; bit16~23
@@ -21,3 +28,31 @@ OSStartHighRdy
  
 	OSStartHang
 	B OSStartHang ; 程序应永远不会运行到这里
+	
+	
+
+PendSV_Handler
+	CPSID	I
+	MRS		R0,PSP
+	CBZ		R0,OS_CPU_PendSVHandler_nosave
+	
+	STMDB	R0!, {R4-R11}
+	LDR		R1, = OSTCBCurPtr
+	LDR		R1, [R1]
+	STR		R0, [R1]
+	
+OS_CPU_PendSVHandler_nosave
+	LDR		R0, = OSTCBCurPtr
+	LDR 	R1, = OSTCBHighRdyPtr
+	LDR		R2, [R1]
+	STR		R2, [R0]
+	
+	LDR 	R0, [R2]
+	LDMIA	R0!, {R4-R11}
+	
+	MSR		PSP, R0
+	ORR		LR, LR,#0x04
+	
+	CPSIE	I
+	
+	BX		LR
